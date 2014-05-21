65dbad3ff868f9492610c8eea4825bf5cf819620
Kevin Umbach <kumbach@gmail.com>
2014-05-21 13:27:05 -0600

    This is the intro sample we did in class.

---
+++ Hello World Sample/HelloWorld/HelloHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+  class HelloHelper
+  {
+    List<string> people = new List<string>();
+
+    internal void addPerson(string name)
+    {
+      if (people.Contains(name))
+      {
+        Console.WriteLine("I'm already going to say hello to " + name);
+      }
+      people.Add(name);
+    }
+
+    protected internal void sayHiToEveryone()
+    {
+      if (people.Count() > 0)
+      {
+
+        Console.WriteLine("\nI'd like to say hello to the following people:");
+        foreach (string person in people)
+        {
+          Console.WriteLine("  - " + person);
+        }
+      }
+      else
+      {
+        Console.WriteLine("\nOkay then, I won't say hello to anyone.");
+      }
+    }
+  }
+}
---
+++ Hello World Sample/HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("math expression: 123.45 X 67.89 = " + 123.45 * 67.89);
+
+      HelloHelper helper = new HelloHelper();
+      string name;
+      for(;;)
+      {
+        Console.Write("Tell me a name: ");
+        name = Console.ReadLine().Trim();
+        if (name.Length == 0) 
+        {
+          break;
+        }
+        helper.addPerson(name);
+      };
+
+      helper.sayHiToEveryone();
+    }
+  }
+}
