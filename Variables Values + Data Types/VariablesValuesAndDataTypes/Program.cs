9979d3b81eeb79896d9319912c08a0c408e2868e
Kevin Umbach <kumbach@gmail.com>
2014-05-21 13:31:50 -0600

    Learning about variables, values, and data types
    Displaying a bunch of information

---
+++ Variables Values + Data Types/VariablesValuesAndDataTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesValuesAndDataTypes.Kevin
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(true && false);
+
+
+
+      char singleDigit = '5';
+      int simpleNumber = 25;
+      string stringDigits = "42";
+      Console.WriteLine((int)30.5);
+
+      Console.WriteLine("'5' " + (int)singleDigit + " " + Convert.ToInt32(singleDigit));
+      Console.WriteLine("25 " + (double)simpleNumber + " " + Convert.ToDouble(simpleNumber));
+      Console.WriteLine("\"42\" " + double.Parse(stringDigits.ToString()) + " " + Convert.ToDouble(stringDigits));
+    }
+  }
+}
