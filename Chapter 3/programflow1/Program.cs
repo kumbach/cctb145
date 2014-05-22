48f19535443a3f85e022f29b6c11068c68aa77c5
Kevin Umbach <kumbach@gmail.com>
2014-05-22 09:44:39 -0600

    added looping samples

--- Chapter 3/programflow1/Program.cs
+++ Chapter 3/programflow1/Program.cs
@@ -12,7 +12,65 @@ namespace programflow1
     {
       Program myApp = new Program();
       myApp.IfStatementSample();
-      Console.WriteLine("Goodbye.");
+      myApp.ForStatementSample();
+      myApp.WhileStatementSamples();
+      myApp.ListDivisibleBy(3);
+      int someNumber;
+      Console.Write("\nEnter a number between 2 and 20: ");
+      someNumber = int.Parse(Console.ReadLine());
+      myApp.ListDivisibleBy(someNumber);
+
+
+      Console.Write("\nPress [ENTER] to exit.");
+      Console.ReadLine();
+    }
+
+    private void ListDivisibleBy(int divisor)
+    {
+      Console.WriteLine("List all the numbers from 1 to 100 that are divisible by {0}", divisor);
+      for (int counter = 1; counter <= 100; counter++)
+      {
+        if (counter % divisor == 0)
+        {
+          Console.Write(counter);
+          if (counter <= 100 - divisor)
+          {
+            Console.Write(", ");
+          }
+        }
+      }
+      Console.WriteLine();
+    }
+
+    private void ForStatementSample()
+    {
+      Console.WriteLine("For Statement Sample...");
+      for (int counter = 1; counter <= 10; counter++)
+      {
+        Console.WriteLine(counter);
+      }
+    }
+
+    private void WhileStatementSamples()
+    {
+      Console.WriteLine("While and Do-While Samples...");
+
+      int counter = 10;
+      while (counter > 0)
+      {
+        Console.WriteLine(counter);
+        counter--;
+      }
+
+      int age;
+      do
+      {
+        Console.Write("Enter your age once more: ");
+        string stringAge = Console.ReadLine();
+        age = int.Parse(stringAge);
+      } while (age < 1 || age > 100);
+      string message = string.Format("You are {0} years old!", age);
+      Console.WriteLine(message);
     }
 
     public void IfStatementSample()
@@ -22,8 +80,12 @@ namespace programflow1
       Console.Write("Enter your age: ");
       string stringAge = Console.ReadLine();
       int age = int.Parse(stringAge);
-
-      if (age >= 18)
+      
+      if (age >= 65)
+      {
+        Console.WriteLine("You are a senior");
+      }
+      else if (age >= 18)
       {
         Console.WriteLine("You are an adult");
       }
